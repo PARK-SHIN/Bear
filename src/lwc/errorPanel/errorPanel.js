2339a615ee73c96a7b151f080fa3d9bc96a47f89
i2max-ShinPark <sp@i2max.co.kr>
2022-07-04 10:43:52 +0900

    2022-07-04 LWC Bike Add2

---
+++ src/lwc/errorPanel/errorPanel.js
@@ -0,0 +1,9 @@
+/**
+ * Created by i2max-ShinPark on 2022-07-04.
+ */
+
+import { LightningElement, api } from 'lwc';
+
+export default class ErrorPanel extends LightningElement {
+  @api errors;
+}
